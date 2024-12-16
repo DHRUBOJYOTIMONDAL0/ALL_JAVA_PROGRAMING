c6e408d5f2d602fee83ce47e1c0af6bea7fd7463
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2024-12-16 12:42:06 +0530

    basic concept about Static variable, methods, Blocks

---
+++ Loops/Demo_staticVariable.java
@@ -0,0 +1,53 @@
+class Mobile{
+    String band; // band and price are instance variables
+    int price;
+   static String name; // static variable share the same value with other obhject
+
+    static {
+        name = "Phone";
+        System.out.println("In static block");
+    }
+
+   public Mobile(){
+    brand = "";
+    price = 200;
+    System.out.println("In constructor");
+   }
+
+    public void show(){
+        System.out.println(band + " : " + price + " : " + name);
+    }
+
+    // public void show1(Mobile obj){ // static method
+    //     System.out.println(obj.band + " : " + obj.price + " : " + name);
+    // }
+    
+    
+}
+
+public class Demo_staticVariable {
+    public static void main(String[] args) throws ClassNotFoundException{
+        
+        // Mobile obj1 = new Mobile();
+        // obj1.band = "Apple";
+        // obj1.price = 1500;
+        // //obj1.name = "SmartPhone";
+        // Mobile.name = "Phone";
+
+        // Mobile obj2 = new Mobile();
+        // obj2.band = "Samsung";
+        // obj2.price = 1800;
+        // //obj2.name = "SmartPhone";
+        // Mobile.name = "Phone";
+
+        // //obj1.name ="Phone";
+        // obj1.show();
+        // obj2.show();
+
+       // Mobile.show1(obj1);
+
+       Class.forName(className:"Mobile")
+    }
+
+
+}
