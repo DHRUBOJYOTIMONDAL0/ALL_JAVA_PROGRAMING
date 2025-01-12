347b39581a0fd1a3137c467a4631a90ab2fed3ff
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-12 17:43:51 +0530

    some

--- arrays/Demo_StringBuffer.java
+++ arrays/Demo_StringBuffer.java
@@ -2,10 +2,14 @@ public class Demo_StringBuffer {
 
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("Dhrubojyoti");
-        sb.append(" Mondal");
+         sb.append(" Mondal");
+         //sb.deleteCharAt(2)
         sb.insert(0, "JAVA ");
         sb.setLength(25);
-        System.out.println(sb);
+         System.out.println(sb);
+        //System.out.println(sb.length());
 
     }
 }
+// string buffer convert to string using toString() method
+// string buffer thread safe but string builder not thread safe
