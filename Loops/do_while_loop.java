4d5e6c6061c619d6ae67179bcc1152ec1b8260c7
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-13 13:06:28 +0530

    update do while loop

--- Loops/do_while_loop.java
+++ Loops/do_while_loop.java
@@ -1,7 +1,7 @@
 public class do_while_loop {
     public static void main(String[] args) {
         int i =1;
-        int i = 5;
+        //int i = 5;
 
         do{
             System.out.println("Hi" + i);
