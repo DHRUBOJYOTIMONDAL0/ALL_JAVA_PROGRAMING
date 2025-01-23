02f622748cf08cc6a481fe35892cea4dd6bb7808
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-23 13:19:16 +0530

    final keyword

---
+++ Methods /Final_Keyword.java
@@ -0,0 +1,14 @@
+public class Final_Keyword {
+
+    public static void main(String[] args) {
+        final int num = 8; //use final keyword you can't change value 
+        num =9;
+        System.out.println(num);
+        
+    }
+}
+
+
+// final keyword - can use in a variable, method, class
+// what is constant - if you make your variable constant you can't change the value of it.
+// now the way you can do that is by using final keyword.
