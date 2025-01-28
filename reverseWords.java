d5471dee60527e72d2b3296bee9346740d96c69e
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-28 21:25:51 +0530

    leetcode

--- reverseWords.java
+++ reverseWords.java
@@ -1,23 +1,3 @@
-// public class Hello2 {
-//     class A{
-//         public void print(){
-//             System.out.println("Hello, World!");
-//         }
-//     }
-//     public static void main(String[] args) {
-//         System.out.println("Hello, World!");
-
-//         Hello2 h = new Hello2();
-//         A a = h.new A();
-//         a.print();
-
-//         A a2 = new Hello2().new A();
-
-//         a2.print();
-//     }
-// }
-
-
 import java.util.Scanner;
 
 class Solution {
